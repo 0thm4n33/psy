f98297e84efdead1433e4fe10a0ed9b83a800209
0thm4n3 <essaidiothmane0@gmail.com>
2022-08-24 15:36:27 +0200

    creating conntactUs Page

---
+++ src/components/forms/LabelTextArea/LabelTextArea.js
@@ -0,0 +1,19 @@
+import { Box, TextField } from "@mui/material";
+import React from "react";
+
+export default function LabelTextArea({label,row,col}){
+    const style = {
+        backgroundColor:"white",
+        borderRadius:"10px",
+        padding:"15px",
+        display:"flex",
+        flexDirection:"column",
+        height:"40%",
+    }
+    return(
+        <Box style={style} sx={{width:{xs:"79%",md:"90%"}}}>
+            <div>{label}</div>
+            <TextField minRows={6} multiline sx={{margin:"20px"}} placeholder={"Ecrivez ici"}/>
+        </Box>
+    )
+}
